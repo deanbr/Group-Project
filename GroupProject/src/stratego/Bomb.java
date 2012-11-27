3ede312a7033f5616f47a0d78fe2183838dc00d3
Brandon Dean <temple747@gmail.com>
2012-11-26 22:42:01 -0500

    same as above

---
+++ GroupProject/src/stratego/Bomb.java
@@ -0,0 +1,24 @@
+package stratego;
+
+/**
+ * // -------------------------------------------------------------------------
+/**
+ * This is the Bomb class, a subclass of GamePiece.
+ *
+ *  @author J
+ *  @version Nov 11, 2012
+ */
+public class Bomb extends GamePiece
+{
+
+    /**
+     * This will create a bomb game piece.
+     * @param x is the x position.
+     * @param y is the y position.
+     * @param team 0 represents blue, 1 represents red.
+     */
+    public Bomb(int x, int y, int team) {
+        super(x, y, team, 11);
+    }
+
+}
---
+++ GroupProject/src/stratego/Flag.java
@@ -0,0 +1,24 @@
+package stratego;
+
+/**
+ * // -------------------------------------------------------------------------
+/**
+ * This is the flag game piece, a subclass of the GamePiece class.
+ *
+ *  @author J
+ *  @version Nov 11, 2012
+ */
+public class Flag extends GamePiece
+{
+
+    /**
+     * This will create a flag game piece.
+     * @param x is the x position.
+     * @param y is the y position.
+     * @param team 0 represents blue, 1 represents red.
+     */
+    public Flag(int x, int y, int team)
+    {
+        super(x, y, team, 12);
+    }
+}
